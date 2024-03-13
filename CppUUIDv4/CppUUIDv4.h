b24b12e1e0d826d8ed0968085bee6d4190d92de2
Leundo <daysundoing@outlook.com>
2024-03-14 00:39:04 +0800

    Initial Commit

---
+++ CppUUIDv4/CppUUIDv4.h
@@ -0,0 +1,18 @@
+//
+//  CppUUIDv4.h
+//  CppUUIDv4
+//
+//  Created by Undo Hatsune on 2024/03/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CppUUIDv4.
+FOUNDATION_EXPORT double CppUUIDv4VersionNumber;
+
+//! Project version string for CppUUIDv4.
+FOUNDATION_EXPORT const unsigned char CppUUIDv4VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CppUUIDv4/PublicHeader.h>
+
+
